04b6fa8971314ea23cb7f9d77869e4a7f008898e
Andi Muhammad Abigail Ghalib Athallah Surya Putra <148310311+AndiAbi4925@users.noreply.github.com>
2025-12-14 22:32:58 +0800

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // 1. Create a "Instance" of axios
 // This basically saves the base URL so we don't have to type it every time
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://3.88.199.161/api',
 });
 
 // 2. The "Interceptor" (The Security Guard)
@@ -16,5 +16,4 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
 export default api;
